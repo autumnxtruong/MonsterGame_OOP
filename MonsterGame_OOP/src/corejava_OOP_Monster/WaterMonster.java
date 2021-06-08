26490aef0cbda30f950db30adb3bfd8ba3e609d4
autumnxtruong <autumnxtruong@gmail.com>
2021-06-08 16:54:24 -0500

    pushing all project files from local

---
+++ MonsterGame_OOP/src/corejava_OOP_Monster/WaterMonster.java
@@ -0,0 +1,12 @@
+package corejava_OOP_Monster;
+
+public class WaterMonster extends Monster {
+	public WaterMonster(String name) {
+		super(name);
+	}
+
+	@Override
+	public String attack() {
+		return "Attack with water!"; 
+	}
+}
